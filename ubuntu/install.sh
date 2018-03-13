0179ca9ce37a194f090c0e781af7500515560ef4
Sarmaad <sarmaad@gmail.com>
2018-03-13 23:33:28 +1100

    wip building settings file

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# usage: install.sh server_url cert_file cert_password
+
 # Ubuntu 16.x
 # RavenDB 4.0 version 4.0.3-patch-40031
 
@@ -9,14 +11,31 @@ if [[ $UID != 0 ]]; then
     exit 1
 fi
 
+# capture all args
+args=("$@")
+
+# exit if no args
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 1
+fi
+
+
 # configuration
-RAVENDB_DL = "https://daily-builds.s3.amazonaws.com/RavenDB-4.0.3-patch-40031-linux-x64.tar.bz2"
+RAVENDB_DL_FILE="RavenDB-4.0.3-patch-40031-linux-x64.tar.bz2"
+RAVENDB_DL="https://daily-builds.s3.amazonaws.com/$RAVENDB_DL_FILE"
 
-RAVENDB_USER = "ravendb"
-RAVENDB_GROUP = "ravendb"
+RAVENDB_USER="ravendb"
+RAVENDB_GROUP="ravendb"
 
-RAVENDB_HOME_DIR = "/home/ravendb"
-RAVENDB_DATA_DIR = "/home/ravendb/data"
+RAVENDB_HOME_DIR="/home/ravendb"
+RAVENDB_DATA_DIR="/home/ravendb/data"
+
+RAVENDB_CONF_FILE="settings.json"
+SERVER_URL=${args[0]}
+
+CERT_FILE=${args[1]}
+CERT_PASS=${args[2]}
 
 # create ravendb user to run the service
 useradd -s /usr/sbin/nologin $RAVENDB_USER $RAVENDB_GROUP
@@ -35,5 +54,19 @@ chown -R $RAVENDB_USER:$RAVENDB_GROUP $RAVENDB_DATA_DIR
 cd $RAVENDB_HOME_DIR
 
 # download ravendb install package and uncompress it
-wget $RAVENDB_DL
+wget $RAVENDB_DL 
+tar xvf $RAVENDB_DL_FILE
 
+# build the settings file
+echo "Creating the settings.json file"
+
+touch $RAVENDB_CONF_FILE
+chmod -R 0600 $RAVENDB_CONF_FILE
+chown -R $RAVENDB_USER:$RAVENDB_GROUP $RAVENDB_CONF_FILE
+
+
+echo "{" >> $RAVENDB_CONF_FILE
+echo "\t\"ServerUrl\"=\"$SERVER_URL\"" >> $RAVENDB_CONF_FILE
+echo "\t\"Setup.Mode\"=\"None\"" >> $RAVENDB_CONF_FILE
+echo "\t\"Setup.Mode\"=\"None\"" >> $RAVENDB_CONF_FILE
+echo "}" >> $RAVENDB_CONF_FILE
